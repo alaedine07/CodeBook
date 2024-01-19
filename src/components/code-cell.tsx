878cb19d58ca09bbcfd7d9e7a286960df0abc3fc
Alaeddine Boufaden <Alaeddine.Boufaden@expensya.com>
2024-01-19 08:53:43 +0100

    add cumulative code execution

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -5,6 +5,7 @@ import Resizable from './resizable';
 import { ICell } from '../state';
 import { useActions } from '../hooks/use-actions';
 import { useTypedSelector } from '../hooks/use-typed-selector';
+import { useCumulativeCode } from '../hooks/use-cumulative-code';
 import './code-cell.css';
 
 interface ICodeCellPros {
@@ -14,28 +15,29 @@ interface ICodeCellPros {
 const CodeCell: React.FC<ICodeCellPros> = ({ cell }) => {
     const { updateCell, createBundle } = useActions();
     const bundle = useTypedSelector((state) => state.bundles[cell.id]);
+    const cumulativeCode = useCumulativeCode(cell.id);
 
     useEffect(() => {
         if (!bundle) {
-            createBundle(cell.id, cell.data);
+            createBundle(cell.id, cumulativeCode);
             return;
         }
         const timer = setTimeout(async () => {
-            createBundle(cell.id, cell.data);
+            createBundle(cell.id, cumulativeCode);
         }, 1000);
 
         return () => {
             clearTimeout(timer);
         };
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [cell.data, cell.id, createBundle]);
+    }, [cumulativeCode, cell.id, createBundle]);
 
     return (
         <Resizable direction='vertical'>
             <div style={{ height: 'calc(100% - 10px)', display: 'flex', flexDirection: 'row' }}>
                 <Resizable direction='horizontal'>
                     <CodeEditor
-                        initialValue={ cell.data }
+                        initialValue={ cumulativeCode }
                         onChange={(value) => updateCell(cell.id, value)}
                     />
                 </Resizable>
