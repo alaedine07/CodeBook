4e8d49fe12306ccabe75ca96f0306a81fb8adcff
alaedine <alaedineboufaden@gmail.com>
2024-01-27 23:50:13 +0100

    adding comments

--- packages/local-client/src/state/reducers/bundlesReducer.ts
+++ packages/local-client/src/state/reducers/bundlesReducer.ts
@@ -2,6 +2,10 @@ import { produce } from "immer";
 import { ActionType } from "../action-types";
 import { Action } from "../actions";
 
+// the IBundlesState interface defines a state is
+// structure where each key is a string (representing the id of a bundle),
+// and the value associated with each key can either be an object with
+// properties loading, code, and err, or it can be undefined.
 interface IBundlesState {
   [key: string]:
     | {
@@ -14,6 +18,9 @@ interface IBundlesState {
 
 const initialState: IBundlesState = {};
 
+// produce: The produce function from the immer library is used to create a
+//  new  state based on the current state with modifications. It simplifies
+// the process of working with immutable state in a more mutable syntax.
 const reducer = produce(
   (state: IBundlesState = initialState, action: Action): IBundlesState => {
     switch (action.type) {
--- packages/local-client/src/state/reducers/index.ts
+++ packages/local-client/src/state/reducers/index.ts
@@ -2,11 +2,20 @@ import { combineReducers } from "redux";
 import cellsReducer from "./cellsReducer";
 import bundlesReducer from "./bundlesReducer";
 
+// The combineReducers function takes an object where each key represents
+// a slice of the Redux state and the corresponding value
+// is the reducer responsible for managing that slice.
 const reducers = combineReducers({
   cells: cellsReducer,
   bundles: bundlesReducer,
 });
 
+// The root reducer (reducers) is exported so that
+// it can be used when creating the Redux store.
 export default reducers;
 
+// The RootState type is exported. It represents the type of the entire
+// Redux state. It is generated using the ReturnType utility type applied to
+// the reducers function. This type is useful for specifying the type of the
+// state when accessing it in components or actions.
 export type RootState = ReturnType<typeof reducers>;
